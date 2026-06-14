b6481a4428050b1f83a0e407092acec6ce031654
Jagger Adams <jadam85@uwo.ca>
2026-06-14 15:39:06 -0400

    api req

--- frontend/src/pages/Overview.jsx
+++ frontend/src/pages/Overview.jsx
@@ -6,24 +6,31 @@ import Loader from '../components/Loader';
 
 export default function Overview() {
   const [loading, setLoading] = useState(true);
+  const [data, setData] = useState({});
+  const url = process.env.REACT_APP_API_URL +"/overview";
+
+  useEffect(() => {
+          async function fetchData() {
+            const response = await fetch(url);
+            const json = await response.json();
+            setData(json);
+            setLoading(false);
+          }
+          fetchData();
+      }, []);
 
   return(
     <div className='overviewRoot'>
       {loading ?
         <Loader text='Fetching latest Data' /> :
         <div>
-          <OverviewContent setLoading={setLoading} />
+          <OverviewContent data={data} />
         </div>
       }
     </div>
   );
 }
 
-function OverviewContent({ setLoading }) {
-    useEffect(() => {
-        console.log("hi")
-        setLoading(false);
-    }, []);
-
-    return <div>content</div>;
+function OverviewContent({ data }) {
+    return <div>data</div>;
 }
